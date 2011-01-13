4aee61f4872875f6e62ad2158df2f45068490428
Emond Papegaaij <emond.papegaaij@topicus.nl>
2011-01-13 16:09:03 +0100

    options for pie chart

--- src/main/java/nl/topicus/wqplot/options/PlotPieRendererOptions.java
+++ src/main/java/nl/topicus/wqplot/options/PlotPieRendererOptions.java
@@ -13,10 +13,67 @@ public class PlotPieRendererOptions implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * diameter of the pie, auto computed by default
+	 */
+	private Double diameter;
+
+	/**
+	 * padding between the pie and plot edges, legend, etc.
+	 */
+	private Double padding;
+
+	/**
+	 * pixels spacing between pie slices.
+	 */
 	private Double sliceMargin;
+
+	/**
+	 * true or false, wether to fil the slices.
+	 */
+	private Boolean fill;
+
+	/**
+	 * offset of the shadow from the slice and offset of each succesive stroke of the
+	 * shadow from the last.
+	 */
+	private Double shadowOffset;
+
+	/**
+	 * transparency of the shadow (0 = transparent, 1 = opaque)
+	 */
+	private Double shadowAlpha;
+
+	/**
+	 * number of strokes to apply to the shadow, each stroke offset shadowOffset from the
+	 * last.
+	 */
+	private Integer shadowDepth;
 
 	public PlotPieRendererOptions()
 	{
+	}
+
+	public Double getDiameter()
+	{
+		return diameter;
+	}
+
+	public PlotPieRendererOptions setDiameter(Double diameter)
+	{
+		this.diameter = diameter;
+		return this;
+	}
+
+	public Double getPadding()
+	{
+		return padding;
+	}
+
+	public PlotPieRendererOptions setPadding(Double padding)
+	{
+		this.padding = padding;
+		return this;
 	}
 
 	public Double getSliceMargin()
@@ -27,6 +84,50 @@ public class PlotPieRendererOptions implements Serializable
 	public PlotPieRendererOptions setSliceMargin(Double sliceMargin)
 	{
 		this.sliceMargin = sliceMargin;
+		return this;
+	}
+
+	public Boolean isFill()
+	{
+		return fill;
+	}
+
+	public PlotPieRendererOptions setFill(Boolean fill)
+	{
+		this.fill = fill;
+		return this;
+	}
+
+	public Double getShadowOffset()
+	{
+		return shadowOffset;
+	}
+
+	public PlotPieRendererOptions setShadowOffset(Double shadowOffset)
+	{
+		this.shadowOffset = shadowOffset;
+		return this;
+	}
+
+	public Double getShadowAlpha()
+	{
+		return shadowAlpha;
+	}
+
+	public PlotPieRendererOptions setShadowAlpha(Double shadowAlpha)
+	{
+		this.shadowAlpha = shadowAlpha;
+		return this;
+	}
+
+	public Integer getShadowDepth()
+	{
+		return shadowDepth;
+	}
+
+	public PlotPieRendererOptions setShadowDepth(Integer shadowDepth)
+	{
+		this.shadowDepth = shadowDepth;
 		return this;
 	}
 }
