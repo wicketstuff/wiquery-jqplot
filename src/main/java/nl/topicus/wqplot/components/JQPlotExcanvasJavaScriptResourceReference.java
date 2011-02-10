cd46444386ff55145e410936e5a0cbdd42e0aad5
Hielke Hoeve <hielke.hoeve@gmail.com>
2011-02-10 15:47:46 +0100

    include IE canvas javascript and resourcereference

---
+++ src/main/java/nl/topicus/wqplot/components/JQPlotExcanvasJavaScriptResourceReference.java
@@ -0,0 +1,26 @@
+package nl.topicus.wqplot.components;
+
+import org.odlabs.wiquery.core.commons.WiQueryJavaScriptResourceReference;
+
+/**
+ * Special javascript for when the User Agent is Internet Explorer.
+ * 
+ * @author hielkehoeve
+ */
+public class JQPlotExcanvasJavaScriptResourceReference extends WiQueryJavaScriptResourceReference
+{
+	private static final long serialVersionUID = -4771815414204892357L;
+
+	private static JQPlotExcanvasJavaScriptResourceReference INSTANCE =
+		new JQPlotExcanvasJavaScriptResourceReference();
+
+	private JQPlotExcanvasJavaScriptResourceReference()
+	{
+		super(JQPlotExcanvasJavaScriptResourceReference.class, "excanvas.js");
+	}
+
+	public static JQPlotExcanvasJavaScriptResourceReference get()
+	{
+		return INSTANCE;
+	}
+}
